d9628968869d548cb197123e880c1b838f532e43
Ranzzik <ranzz773@gmail.com>
2025-06-15 15:12:03 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,6 +1,6 @@
 # MIT License
 
-**Copyright (c) 2023 Ranzzik**
+**Copyright (c) 2025 Ranzzik**
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
